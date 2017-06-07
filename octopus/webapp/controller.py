3f02e31a5ab12b562074914da7d45ce6b435c532
Hunchul Park <huntrax11@gmail.com>
2017-06-07 19:27:16 +0900

    Cont'd dd2067804ba0

--- octopus/webapp/controller.py
+++ octopus/webapp/controller.py
@@ -1,17 +1,22 @@
-from flask import Flask
-from flask import render_template
-
-from octopus.ml.clustering import KMeansClustering
+from flask import Flask, render_template, abort
 from octopus.sns import get_articles, get_user_likes_map
+from octopus.ml.clustering import KMeansClustering
 
 app = Flask(__name__)
 app.debug = True
+
+
+@app.route('/favicon.ico')
+def favicon():
+    abort(404)
 
 
 @app.route('/')
 @app.route("/<username>")
 def visualize_ml(username='huntrax11'):
     articles = get_articles(username)
+    if not articles:
+        abort(404)
     user_likes_map = get_user_likes_map(username)
     k = KMeansClustering(user_likes_map)
     nodes = []
@@ -20,7 +25,7 @@ def visualize_ml(username='huntrax11'):
         for user, distance in zip(cluster.get('users'), cluster['distances']):
             nodes.append({
                 "cluster": i,
-                "radius": (min_distance+1)/(distance+1)*25,
+                "radius": (min_distance+1)/(distance+1)*30,
                 "icon": user.profile_pic_url,
                 "name": user.username
             })
